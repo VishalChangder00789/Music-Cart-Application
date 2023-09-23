e150a6051218f9307d52a79af1ef2ec88766e933
Bishal Changder <changdervishal1999@gmail.com>
2023-09-23 20:53:10 +0530

    Constant paths are added for better and faster developements

---
+++ client/src/Constants/Server_Path.js
@@ -0,0 +1,8 @@
+export const GET_ALL_PRODUCTS = "http://localhost:8000/api/v1/_PRODUCTS";
+export const GET_PRODUCT_BY_ID = (id) => {
+  return `http://localhost:8000/api/v1/_PRODUCTS/${id}`;
+};
+export const UPDATE_PRODUCT_BY_ID = (id) => {
+  return `http://localhost:8000/api/v1/_PRODUCTS/${id}`;
+};
+export const CREATE_PRODUCT = "http://localhost:8000/api/v1/_PRODUCTS";
